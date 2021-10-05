0cc52bd2d13cd2361b0659c9f3a144ae0e4babaf
lucasmss <lucasmss16@gmail.com>
2021-10-05 11:49:59 -0300

    Tela de cadastro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,11 +9,23 @@ export class LoginComponent {
 
   usuario: string;
   password: string;
+  loginErro: boolean;
+  cadastrando: boolean;
+  
 
   constructor() { }
 
   entrar(){
     
+  }
+
+  cadastrar(event){
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelarCadastro(){
+    this.cadastrando = false;
   }
 
 }
