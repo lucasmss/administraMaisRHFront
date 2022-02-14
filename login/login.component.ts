da71e6e463a479f75004e1bdfb2d75c0625be2e1
Lucas Mateus Silva <42013706+lucasmss@users.noreply.github.com>
2022-02-14 17:26:27 -0300

    Add files via upload

---
+++ login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../shared/models/usuario/usuario';
+import { AuthService } from '../shared/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  username: string;
+  password: string;
+  errors: String[];
+  mensagemSucesso: string;
+  cadastrando: boolean;
+  
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  entrar(){
+    this.router.navigate(['/home']);    
+  }
+  
+  cadastrar(event){
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelarCadastro(){
+    this.cadastrando = false;
+  }
+
+  cadastrarUsuario(){
+    const username: Usuario = new Usuario();
+    username.username = this.username;
+    username.password = this.password;
+    this.authService
+        .salvar(username)
+        .subscribe( response => {
+          this.mensagemSucesso = "Cadastro realizado com sucesso!";
+          this.errors = null;
+        }, errorResponse => {
+          this.mensagemSucesso = null;
+          this.errors = errorResponse.error.erros;  
+        })
+  }
+
+}
---
+++ shared/models/servicos/servicos.ts
@@ -0,0 +1,6 @@
+export class Servicos {
+    descricao!: string;
+    preco!: string;
+    data!: string;
+    idCliente!: string;
+}
